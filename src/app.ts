459c9bfbd56b7443ad4b3fd3176cd66519a06694
a28689604 <a28689604@gmail.com>
2022-12-18 00:58:53 +0800

    basic finish

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,32 @@
-// Code goes here!
+import axios from "axios";
+
+const form = document.querySelector("form")!;
+const apiInput = document.getElementById("api")! as HTMLInputElement;
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+type GoogleGeocodingRes = { results: { geometry: { location: { lat: number; lng: number } } }[]; status: "OK" | "ZERO_RESULTS" };
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+  const apiKey = apiInput.value;
+
+  axios
+    .get<GoogleGeocodingRes>(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(enteredAddress)}&key=${apiKey}`)
+    .then((res) => {
+      if (res.data.status !== "OK") {
+        throw new Error("Could not fetch location");
+      }
+      const coord = res.data.results[0].geometry.location;
+      const map = new google.maps.Map(document.getElementById("map") as HTMLElement, {
+        center: coord,
+        zoom: 8,
+      });
+      new google.maps.Marker({ position: coord, map: map });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+
+form?.addEventListener("submit", searchAddressHandler);
